47c94c05504ad4e53c4f14c92f57322864353917
WolfTechAustria <wo@wolftech.at>
2026-09-12 00:42:22 +0200

    changed editor loading

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,65 @@ import Quill from 'quill';
 import 'quill/dist/quill.snow.css';
 
 window.Quill = Quill;
+
+document.addEventListener('alpine:init', () => {
+
+    Alpine.data('templateEditor', (bodyHtml) => ({
+
+        quill: null,
+        bodyHtml: bodyHtml,
+
+        init() {
+
+            this.quill = new Quill(
+                this.$refs.editor,
+                {
+                    theme: 'snow',
+
+                    modules: {
+                        toolbar: [
+                            ['bold', 'italic', 'underline'],
+                            [{ size: ['small', false, 'large', 'huge'] }],
+                            [{ color: [] }],
+                            [{ align: [] }],
+                            [{ list: 'ordered' }, { list: 'bullet' }],
+                            ['clean']
+                        ]
+                    }
+                }
+            );
+
+            this.quill.root.innerHTML =
+                this.bodyHtml || '';
+
+            this.quill.on('text-change', () => {
+
+                this.bodyHtml =
+                    this.quill.root.innerHTML;
+
+                this.$wire.set(
+                    'bodyHtml',
+                    this.bodyHtml,
+                    false
+                );
+            });
+
+            this.$watch(
+                'bodyHtml',
+                (value) => {
+
+                    const html = value || '';
+
+                    if (
+                        this.quill &&
+                        this.quill.root.innerHTML !== html
+                    ) {
+                        this.quill.root.innerHTML = html;
+                    }
+                }
+            );
+        }
+
+    }));
+
+});
